02d1f3fda0e16f527c8f1b23d3fb06e9546f6232
Hari Sundar <hari@ices.utexas.edu>
2014-06-05 19:06:46 -0500

    initial

---
+++ contact.markdown
@@ -0,0 +1,8 @@
+---
+title: Contact
+---
+
+Hari Sundar
+hsundar @ ices . utexas . edu
+
+732-672-1094
---
+++ site.hs
@@ -0,0 +1,108 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Binary (Binary)
+import           Data.Typeable
+import           Data.Monoid (mappend, mconcat)
+import qualified Data.Set as S
+import           Text.Pandoc.Options
+import           Hakyll
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyllWith config $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*.scss" $ do
+        route $ setExtension "css"
+        compile sassCompiler
+
+    match "css/*.css" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match (fromList ["about.rst", "contact.markdown"]) $ do
+        route   $ setExtension "html"
+        compile $ pandocMathCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+						
+    match "teaching/*" $ do
+        route   $ setExtension "html"
+        compile $ pandocMathCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ pandocMathCompiler
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+
+    create ["archive.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let archiveCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Archives"            `mappend`
+                    defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let indexCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Home"                `mappend`
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+sassCompiler :: Compiler (Item String)
+sassCompiler =
+    getResourceString
+        >>= withItemBody (unixFilter "sass" ["-s", "--scss"])
+        >>= return . fmap compressCss
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+pandocMathCompiler =
+    let mathExtensions = [Ext_tex_math_dollars, Ext_tex_math_double_backslash,
+                          Ext_latex_macros]
+        defaultExtensions = writerExtensions defaultHakyllWriterOptions
+        newExtensions = foldr S.insert defaultExtensions mathExtensions
+        writerOptions = defaultHakyllWriterOptions {
+                          writerExtensions = newExtensions,
+                          writerHTMLMathMethod = MathJax ""
+                        }
+    in pandocCompilerWith defaultHakyllReaderOptions writerOptions
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+config :: Configuration
+config = defaultConfiguration
+        {   deployCommand = "rsync -avz -e ssh ./_site/ shell.cs.utah.edu:~/public_html/"}
+
+--------------------------------------------------------------------------------
